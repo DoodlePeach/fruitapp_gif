574001e6ce4dff17a6d1ba9131d9e43977614132
DoodlePeach <31486393+DoodlePeach@users.noreply.github.com>
2021-03-16 13:11:42 +0500

    Added fruit assets and thier registery.

---
+++ lib/assets.dart
@@ -0,0 +1,55 @@
+final basePath = "assets/fruits/";
+final Map fruits = {
+  "watermelon": {
+    "image": "watermelon/1.png",
+    "variants": [
+      {"red": "1.png"},
+      {"white": "2.png"},
+      {"green": "3.png"},
+      {"grey": "4.png"},
+      {"black": "5.png"},
+      {"blue": "6.png"},
+      {"orange": "7.png"},
+      {"yello": "8.png"}
+    ]
+  },
+  "apple": {
+    "image": "apple/1.png",
+    "variants": [
+      {"red": "1.png"},
+      {"white": "2.png"},
+      {"green": "4.png"},
+      {"grey": "8.png"},
+      {"black": "5.png"},
+      {"blue": "7.png"},
+      {"orange": "6.png"},
+      {"yellow": "3.png"}
+    ]
+  },
+  "banana": {
+    "image": "banana/1.png",
+    "variants": [
+      {"red": "6.png"},
+      {"white": "4.png"},
+      {"green": "5.png"},
+      {"grey": "7.png"},
+      {"black": "3.png"},
+      {"blue": "8.png"},
+      {"orange": "2.png"},
+      {"yellow": "1.png"}
+    ]
+  },
+  "pear": {
+    "image": "pear/1.png",
+    "variants": [
+      {"red": "2.png"},
+      {"white": "6.png"},
+      {"green": "1.png"},
+      {"grey": "7.png"},
+      {"black": "3.png"},
+      {"blue": "8.png"},
+      {"orange": "5.png"},
+      {"yellow": "4.png"}
+    ]
+  },
+};
